144cf493e578847b5b72f6bbb492d403aa78dc18
Gavin Clement Melanson <gavincm.developer@gmail.com>
2021-03-08 06:27:10 -0500

    added all mesh objects to PrimitiveMesh class. now any primitive mesh object can be generated

--- scripts/main.py
+++ scripts/main.py
@@ -2,5 +2,3 @@ import import_blender_text as ib
 
 import_alt = ib.import_text_as_mod
 bp = import_alt("blender_primitives")
-
-cube = bp.BlendObject(cube=True)
--- scripts/modules/objects/blender_primitives.py
+++ scripts/modules/objects/blender_primitives.py
@@ -1,21 +1,57 @@
 import bpy as b
 
 
-class BlendObject:
+class PrimitiveMesh:
     def __init__(self, **object_type):
         self.object_type = dict(object_type)
-        self.possible_types = ["cube", "uv_sphere"]
-        if self.object_type.get(self.possible_types[0]):
+        self.possible_primitives = [
+            "cube", "uv_sphere",
+            "circle", "cone",
+            "gizmo_cube", "cylinder",
+            "grid", "ico_sphere",
+            "plane", "torus"
+        ]
+        # using **object_type to create the desired mesh
+        if self.object_type.get(self.possible_primitives[0]):
             self.initialize = b.ops.mesh.primitive_cube_add()
-        if self.object_type.get(self.possible_types[1]):
+
+        if self.object_type.get(self.possible_primitives[1]):
             self.initialize = b.ops.mesh.primitive_uv_sphere_add()
+
+        if self.object_type.get(self.possible_primitives[2]):
+            self.initialize = b.ops.mesh.primitive_circle_add()
+
+        if self.object_type.get(self.possible_primitives[3]):
+            self.initialize = b.ops.mesh.primitive_cone_add()
+
+        if self.object_type.get(self.possible_primitives[4]):
+            self.initialize = b.ops.mesh.primitive_cube_add_gizmo()
+
+        if self.object_type.get(self.possible_primitives[5]):
+            self.initialize = b.ops.mesh.primitive_cylinder_add()
+
+        if self.object_type.get(self.possible_primitives[6]):
+            self.initialize = b.ops.mesh.primitive_grid_add()
+
+        if self.object_type.get(self.possible_primitives[7]):
+            self.initialize = b.ops.mesh.primitive_ico_sphere_add()
+
+        if self.object_type.get(self.possible_primitives[8]):
+            self.initialize = b.ops.mesh.primitive_plane_add()
+
+        if self.object_type.get(self.possible_primitives[9]):
+            self.initialize = b.ops.mesh.primitive_torus_add()
+
         self.__name__ = b.context.active_object
 
     def __repr__(self):
         print(self.__name__)
 
-    def scale_evenly(self, scale_modifier):
+    def even_scale(self, scale_modifier):
         self.__name__.scale = [scale_modifier, scale_modifier, scale_modifier]
 
-    def position_self(self, x, y):
-        self.__name__.location = [x, y, 0.5]
+    def odd_scale(self, x, y, z):
+        self.__name__.scale = [float(x), float(y), float(z)]
+
+    def position_self(self, x, y, z):
+        self.__name__.location = [float(x), float(y), float(z)]
